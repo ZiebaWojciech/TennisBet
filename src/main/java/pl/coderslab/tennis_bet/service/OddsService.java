600a880ee296440714d1c02cdbd132777a549649
Wojciech Zięba <34630361+ZiebaWojciech@users.noreply.github.com>
2018-09-17 13:54:26 +0200

    Odds calculating method

--- src/main/java/pl/coderslab/tennis_bet/service/OddsService.java
+++ src/main/java/pl/coderslab/tennis_bet/service/OddsService.java
@@ -1,8 +1,11 @@
 package pl.coderslab.tennis_bet.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderslab.tennis_bet.sportDataFeed.entity.AtpRankingPosition;
 import pl.coderslab.tennis_bet.sportDataFeed.entity.Event;
+import pl.coderslab.tennis_bet.sportDataFeed.entity.Player;
+import pl.coderslab.tennis_bet.sportDataFeed.service.AtpRankingPositionService;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,14 +16,26 @@ import java.util.List;
 
 @Service
 public class OddsService {
-    
-    private BigDecimal prematchOddsCalculate(Event event) {
-        List<AtpRankingPosition> playerOneAtpRankings = event.getPlayerOne().getAtpRankingPositions();
-        List<AtpRankingPosition> playerTwoAtpRankings = event.getPlayerTwo().getAtpRankingPositions();
+    private final AtpRankingPositionService atpRankingPositionService;
 
-        event.getPlayerOne().getAtpRankingPositions().stream()
+    @Autowired
+    public OddsService(AtpRankingPositionService atpRankingPositionService) {
+        this.atpRankingPositionService = atpRankingPositionService;
+    }
+
+    private BigDecimal prematchOddsCalculate(Event event) {
+        Player playerOne = event.getPlayerOne();
+        Player playerTwo = event.getPlayerTwo();
+
+        AtpRankingPosition playerOneCurrentAptRanking = atpRankingPositionService.getAllByPlayerId(playerOne.getId()).stream()
                 .sorted( (a,b) ->  b.getDate().isAfter(a.getDate())? 1 : 0 )
-                .findFirst();
+                .findFirst()
+                .orElse(null);
+
+        AtpRankingPosition playerTwoCurrentAptRanking = atpRankingPositionService.getAllByPlayerId(playerTwo.getId()).stream()
+                .sorted( (a,b) ->  b.getDate().isAfter(a.getDate())? 1 : 0 )
+                .findFirst()
+                .orElse(null);
 
         return null;
 
